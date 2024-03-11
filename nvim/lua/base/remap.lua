bf059bbaec940b385deba1a09a7821df48ef57d0
Noah wallentin <noah.wallentin@icloud.com>
2024-03-11 18:29:08 +0100

    Added nvim Remap && picom.conf && new fehbg

--- nvim/lua/base/remap.lua
+++ nvim/lua/base/remap.lua
@@ -1,10 +1,10 @@
 
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
@@ -23,6 +23,7 @@ vim.keymap.set("i", "<C-c>", "<Esc>")
 
 -- save with ctrl+s instead of :w
 vim.keymap.set("i", "<C-s>", "<Esc>:w<Enter>a")
+vim.keymap.set("n", "<C-s>", ":w<Enter>")
 
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
